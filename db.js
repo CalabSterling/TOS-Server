b2769a519f089bad1296bf2ce5d3b003805b0c2a
Calab Sterling <calabsterling@gmail.com>
2021-11-16 16:31:21 -0500

    server

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL, {
+const db = new Sequelize(process.env.DATABASE_URL || `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/Red-Badge-Server`, {
   dialect: 'postgres',
-//   dialectOptions: {
-//     ssl: {
-//         require: true,
-//         rejectUnauthorized: false,
-//     }
-// }
+  dialectOptions: {
+    ssl: {
+        require: true,
+        rejectUnauthorized: false,
+    }
+}
 })
 
 module.exports = db;

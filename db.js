15a231bc2867a683b008175de3a64007b15e8c6a
Calab Sterling <calabsterling@gmail.com>
2021-11-16 15:44:22 -0500

    server

--- db.js
+++ db.js
@@ -1,20 +1,22 @@
-const Sequelize = require('sequelize');
+const { Sequelize } = require("sequelize");
+
 const db = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false,
-        }
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+        require: true,
+        rejectUnauthorized: false,
     }
+}
 });
 
-sequelize.authenticate().then(
-    function() {
-        console.log('Connected to database');
-    },
-    function(err){
-        console.log(err);
-    }
+db.authenticate().then(
+  function() {
+      console.log('Connected to database');
+  },
+  function(err){
+      console.log(err);
+  }
 )
+
 module.exports = db;
